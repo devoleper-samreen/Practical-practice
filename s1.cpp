055d7d8beda294b67422bd9351efecabbfeddb32
devoleper-samreen <samreenmalik52292@gmail.com>
2025-01-21 17:50:02 +0530

    Add Student and WorkingStudent classes with display functionality

---
+++ s1.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+    public:
+    string name;
+    int age;
+    string course;
+
+    Student(string name, int age, string course){
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+
+    virtual void display(){
+        cout << "Student Information:\n";
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Course: " << course << endl;
+    }
+}
+
+class WorkingStudent: public Student{
+    private:
+    string companyName;
+    float salary;
+
+    public:
+    WorkingStudent(string name, int age, string course, string companyName, float salary){
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        this.companyName = companyName;
+        this.salary = salary;
+
+    }
+
+    void display override(){
+        cout << "Working Student Information:\n";
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Course: " << course << endl;
+        cout << "Company Name: " << companyName << endl;
+        cout << "Salary: " << salary << endl;
+    }
+
+}
+
+int main(){
+    WorkingStudent workingStudent( "John", 25, "B.Tech", "Google", 100000);
+
+    workingStudent.display();
+
+    return 0;
+}
